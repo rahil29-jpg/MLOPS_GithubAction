75f31a49f4ab275873430939cf9259fa1a3c9ac3
Rahil Shaikh <rahil29@somaiya.edu>
2026-03-17 16:26:32 +0530

    Testing actions on my fork

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,7 +4,7 @@ from src.math_operations import add,sub,mul
 def test_add(): 
     assert add(2,3)==5 #assert add(1,1)==2
     assert add(-1,1)==0
-    
+    assert add(12,13)==25
 def test_sub():
     assert sub(5,3)==2 #assert sub(2,1)==1
     assert sub(4,3)==1
